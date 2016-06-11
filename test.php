a9ef15e09546c18429153f738bd69b4bb065a46f
Goody99 <abdou.lahna@gmail.com>
2016-06-11 18:43:20 -0400

    Add files via upload

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta content="text/html";charset="utf-8" http-equiv="content-type">
+    <title>first PHP page</title>
+</head>
+
+<body>
+
+
+
+<?php
+//my first php page
+phpinfo();
+
+?>
+
+</body>
+
+</html>
