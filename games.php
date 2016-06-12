486171ea24c50a9cbac6ef2f9b1f8d403648609c
Goody99 <abdou.lahna@gmail.com>
2016-06-12 19:04:29 -0400

    Add files via upload

---
+++ games.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta content="text/html"; charset="utf-8" http-equiv="content-type">
+        <title> video games site </title>
+    </head>
+    <body>
+        <h1>Game details</h1>
+        <form action="save-game.php"method="post">
+            <fieldset>
+                <label for="name">Name:</label>
+                <input name="name"/>
+            </fieldset>
+            <fieldset>
+                <label for="age_limit">Age limit:</label>
+                <input name="age_limit"/>
+            </fieldset>
+            <fieldset>
+                <label for="type">Type:</label>
+                <input name="type"/>
+            </fieldset>
+
+        </form>
+    </body>
+
+</html>
