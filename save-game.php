a99c5e7f7b6c0ee5458bbcd3976c66b70efe939a
Goody99 <abdou.lahna@gmail.com>
2016-06-14 00:28:58 -0400

    Add files via upload

--- save-game.php
+++ save-game.php
@@ -14,16 +14,15 @@ $release_date=$_POST['type'];
 $conn=new PDO('mysql:host=sql.computerstudi.es;dbname=gc200325390','gc200325390','DBjvinA?');
 
 //set up an SQL command to save the new game
-$sql ="INSERT INTO games(name,age_limit,type) VALUES(:name,
-:age_limit,:type)";
+$sql ="INSERT INTO games(name,age_limit,type) VALUES(:name,:age_limit,:type)";
 
 // set up command object
 $cmd=$conn ->prepare($sql);
 
 //fill the placeholder with the input variables
-$cmd->bindParam(:name,$name,PDO::PARAM_STR,50);
-$cmd->bindParam(:age_limit,$age_limit,PDO::PARAM_INT);
-$cmd->bindParam(:type,$type,PDO::PARAM_INT);
+$cmd->bindParam(':name',$name,PDO::PARAM_STR,50);
+$cmd->bindParam(':age_limit',$age_limit,PDO::PARAM_INT);
+$cmd->bindParam(':type',$type,PDO::PARAM_INT);
 
 //execute Method
 
