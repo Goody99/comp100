fc36870b1816cedc6f404d5e203d688f973d09f4
Goody99 <abdou.lahna@gmail.com>
2016-07-13 15:27:30 -0400

    Add files via upload

--- classField.php
+++ classField.php
@@ -1,17 +1,36 @@
 
-<?php
-class HTMLBuilder{
-    public function selectBox($name,$collection){
-        $html="<select name='{$name}'>";
-        foreach ($collection as $item){
-            $html.="<option>{$item}</option>";
-        }
-        $html.="</select>";
-        return $html;
-    }
-}
-//instantiated object
-$select= new HTMLBuilder;
-echo $select->selectBox('cities',['orillia','Barrie']);
-$select= new HTMLBuilder;
-echo $select->selectBox('provinces',['ON','QC']);
+
+
+<!DOCTYPE html>
+    <html>
+        <body>
+            <?php
+                class HTMLBuilder{
+                    public function selectBox($name,$collection)
+                    {
+                        $html = "<select name='{$name}'>";
+                        foreach ($collection as $item) {
+                            $html .= "<option>{$item}</option>";
+
+                        $html .= "</select>";
+                        return $html;
+                    }
+
+                //instantiated object
+                    $select= new HTMLBuilder;
+                    echo $select->selectBox('cities',['orillia','Barrie']);
+                    $select= new HTMLBuilder;
+                    echo $select->selectBox('provinces',['ON','QC']);
+
+            ?>
+
+            <form>
+
+                <input type="submit" value="Submit">
+
+            </form>
+
+        </body>
+    </html>
+
+
