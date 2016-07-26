414916b5aef2261050038be2e9dd522c4f5ea2af
Goody99 <abdou.lahna@gmail.com>
2016-07-25 21:52:50 -0400

    Add files via upload

--- Add_team.php
+++ Add_team.php
@@ -4,5 +4,24 @@ echo '<pre>', var_dump($_POST),'</pre>';
 $team_name = $_POST['name'];
 $team_category = $_POST['team_category'];
 $website= $_POST['website'];
+// connection to database
+$dbh = new PDO("mysql:host=localhost;dbname=dbteams","root","root");
+$dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+//Build the SQL
+$sql= 'INSERT INTO teams (name,team_category,website)VALUES(:name,:team_category,:website)';
+
+// prepare the SQL
+
+$sth = $dbh-> prepare ($sql);
+$sth ->bindParam(':name',$team_name, PDO::PARAM_STR,50);
+$sth ->bindParam(':team_category',$team_category, PDO::PARAM_STR,50);
+$sth ->bindParam(':website',$website, PDO::PARAM_STR,100);
+
+// Execute the SQL
+
+$sth->execute();
+
+// close our connection
+$dbh= null;
 ?>
