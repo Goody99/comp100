b3e3a4748dd8752baf57f4b0e2923f393ba5c6e4
Goody99 <abdou.lahna@gmail.com>
2016-08-16 16:30:57 -0400

    Add files via upload

--- Tester.php
+++ Tester.php
@@ -16,6 +16,7 @@
                     $Abdel = new Employee( 'Abdel', 'Lahna', '1979-12-16','1300');
                     var_dump( $Abdel->age());
                     var_dump( $Abdel->net_income());
+                
 
 
 
