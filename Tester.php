c6bad8e146bb92742fcadc7e471a7b5bc802e191
Goody99 <abdou.lahna@gmail.com>
2016-08-16 16:25:48 -0400

    Add files via upload

--- Tester.php
+++ Tester.php
@@ -15,7 +15,7 @@
                     require_once 'employee.php';
                     $Abdel = new Employee( 'Abdel', 'Lahna', '1979-12-16','1300');
                     var_dump( $Abdel->age());
-                    
+                    var_dump( $Abdel->net_income());
 
 
 
